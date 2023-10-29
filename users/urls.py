61947457ef4f40796bbe895f7a3c51c5c4e83385
AndyGarcia <andyg.oyarzo@gmail.com>
2023-10-29 01:43:51 -0300

    Autenticación de usuarios backend

--- users/urls.py
+++ users/urls.py
@@ -5,5 +5,5 @@ from rest_framework_simplejwt.views import TokenRefreshView
 urlpatterns = [
     path('register/', views.register),
     path('login/', views.LoginView.as_view()),
-    path('refresh/', views.LoginView.as_view()),
+    path('refresh/', TokenRefreshView.as_view()),
 ]
